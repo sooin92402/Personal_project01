e8edeebab3e421a01dea6fa2daf9f21d711785d5
sion <sooin92402@gmail.com>
2021-12-17 16:43:31 +0900

    check project working

--- src/main/java/project/myTravel/MyTravelApplication.java
+++ src/main/java/project/myTravel/MyTravelApplication.java
@@ -2,8 +2,9 @@ package project.myTravel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class MyTravelApplication {
 
 	public static void main(String[] args) {
