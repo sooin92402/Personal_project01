4e904cbc4657863e465ad744f98d876b29136e09
sion <sooin92402@gmail.com>
2021-12-17 13:20:16 +0900

    create project

---
+++ src/main/java/project/myTravel/MyTravelApplication.java
@@ -0,0 +1,13 @@
+package project.myTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/myTravel/MyTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package project.myTravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
