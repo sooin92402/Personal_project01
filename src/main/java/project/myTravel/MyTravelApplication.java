cb87ac345a7f3b4a391d4d561b7d9f0a22f3f3e2
sion <sooin92402@gmail.com>
2021-12-17 16:47:19 +0900

    Add explain DB ignore code

--- src/main/java/project/myTravel/MyTravelApplication.java
+++ src/main/java/project/myTravel/MyTravelApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})//exclude : db연결 전까지만 사용 연결후 코드 삭제 필요
 public class MyTravelApplication {
 
 	public static void main(String[] args) {
